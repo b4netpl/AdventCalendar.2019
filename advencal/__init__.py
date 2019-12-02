e513871a914e190bce20fa435f316e0e9eea24a8
Bartek Brzeziński <bartosz.brzezinski@pracuj.pl>
2019-12-02 03:36:23 +0100

    Popups!

--- advencal/__init__.py
+++ advencal/__init__.py
@@ -39,23 +39,65 @@ def create_app():
             # Reaching to db for every cell is sub-optimal, to say the least,
             # but I'm in a hurry. :( This call should be moved to index() def 
             # and the result put into global var.
+            
+            day_id = str(row * 4 + col + 1)
+            
             db = get_db()
             day_data = db.execute(
-                'SELECT * FROM day WHERE id = ' + str(row * 4 + col + 1)
+                'SELECT * FROM day WHERE id = ' + day_id
             ).fetchone()
-            img_url = url_for('static', filename='unc_' + str(row) + '_' + str(col) + '.png')
-            retval = '<td class="cell" style="background-image: url(' + img_url + ');"><a href=# class="uncovered">' + str(day_data['day_no']) + '</a></td>'
+            is_discovered = db.execute(
+                'SELECT * FROM discovered_days WHERE day_id = ' + day_id + ' AND user_id = ' + str(session.get('user_id'))
+            ).fetchone()
+            # if its discovered change img
+            if is_discovered is None:
+                img_url = url_for('static', filename=app.env + '/unc_' + str(row) + '_' + str(col) + '.png')
+            else:
+                img_url = url_for('static', filename=app.env + '/disc_' + str(row) + '_' + str(col) + '.png')
+            retval = '<td class="cell" style="background-image: url(' + img_url + ');">'
+            # set link based on date, discovered and quest
+            custom_popups = {}
+            if is_discovered is None:
+                if date.today().day < day_data['day_no']:
+                    retval += '<a href="#notyetpopup" class="notyet">' + str(day_data['day_no']) + '</a>'
+                elif date.today().day >= day_data['day_no']:
+                    retval += '<a href="#discoverpopup' + day_id + '" class="undiscovered">' + str(day_data['day_no']) + '</a>'
+                    custom_popups[day_id] = {
+                        'type': 'discoverpopup' + day_id,
+                        'quest': day_data['quest']
+                    }
+                # TODO late discover!
+                #else:
+                #    retval += '?'
+            
+            retval += '</td>'
+            for popup in custom_popups:
+                retval += (
+                    '<div id="' + custom_popups[popup]['type'] + '" class="overlay">' +
+                    '<div class="popup"><h2>Tym razem się udało!</h2>' +
+                    '<a class="close" href="#">&times;</a>' +
+                    '<div class="content"><form method="post"><input type="hidden" id="day_id" name="day_id" value="' + day_id + '"><input type="submit" value="Odkryj"></form></div>' +
+                    '</div></div>'
+                )
             return retval
         return dict(get_image=get_image)
 
-    @app.route('/')
+    @app.route('/', methods=('GET', 'POST'))
     # pylint: disable=unused-variable
     def index():
 
         if session.get('user_id') is None:
             return redirect(url_for('login'))
         else:
+            if request.method == 'POST':
+                db = get_db()
+                # TODO if quest check answer
+                db.execute(
+                    'INSERT INTO discovered_days (day_id, user_id, answered) VALUES (' + request.form['day_id'] + ', ' + str(session['user_id']) + ', true)'
+                )
+                db.commit()
             return render_template('calendar.html')
+        
 
     @app.route('/login', methods=('GET', 'POST'))
     # pylint: disable=unused-variable
