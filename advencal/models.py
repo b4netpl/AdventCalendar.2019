e745805c6341771614309b93b4668015fbd86fcd
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-11-21 04:01:05 +0100

    added helper methods to User, changed m2m relation

--- advencal/models.py
+++ advencal/models.py
@@ -1,28 +1,38 @@
+from werkzeug.security import check_password_hash, generate_password_hash
 from advencal import db
 
 
-discovered_days = db.Table(
-        'discovered_days',
-        db.Column('id', db.Integer, primary_key=True, autoincrement=True),
-        db.Column('day_id', db.Integer, db.ForeignKey('day.id')),
-        db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
-)
+class DiscoveredDays(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    day_id = db.Column(db.Integer, db.ForeignKey('day.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', back_populates='days')
+    day = db.relationship('Day', back_populates='users')
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    username = db.Column(
+            db.String(64),
+            index=True,
+            unique=True,
+            nullable=False
+            )
     password = db.Column(db.String(128), nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
-    days_discovered = db.relationship(
-            'Day',
-            secondary=discovered_days,
-            lazy='subquery',
-            backref=db.backref('users', lazy=True)
-    )
+    days = db.relationship('DiscoveredDays', back_populates='user')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def get_user(id):
+        return User.query.get(int(id))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Day(db.Model):
@@ -31,6 +41,10 @@ class Day(db.Model):
     quest = db.Column(db.Text)
     quest_answer = db.Column(db.Text)
     hour = db.Column(db.Time, nullable=False, default="00:00:00")
+    users = db.relationship('DiscoveredDays', back_populates='day')
 
     def __repr__(self):
         return '<Day {}>'.format(self.day_no)
+
+    def get_day(id):
+        return Day.query.get(int(id))
