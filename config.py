3f25a41956932aa78db04b767446ce6dcfd80150
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-11-23 04:13:45 +0100

    removed old DB config

--- config.py
+++ config.py
@@ -9,5 +9,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # TODO remove after migrating to SQLAlchemy
-    DATABASE = os.path.join(basedir, 'instance', 'advencal.sqlite')
