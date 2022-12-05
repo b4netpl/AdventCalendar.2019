0dbece92f049c43e68df8c24c6e729dc40af85ce
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-12-05 02:51:51 +0100

    fixed request calls to use url_for()

--- tests/functional/test_index.py
+++ tests/functional/test_index.py
@@ -1,10 +1,13 @@
+from flask import url_for
+
+
 def test_index_nologin(client):
     """
     GIVEN a Flask app configured for testing
     WHEN the '/' page is requested (GET)
     THEN check that response is valid
     """
-    response = client.get('/', follow_redirects=True)
+    response = client.get(url_for('basic.index'), follow_redirects=True)
     assert response.request.path == '/login'
     assert b'2022 B4Team' in response.data
 
@@ -15,6 +18,6 @@ def test_login(client):
     WHEN the '/' page is requested (POST)
     THEN check that response is 405
     """
-    response = client.get('/login')
+    response = client.get(url_for('basic.login'))
     assert response.status_code == 200
     assert b'2022 B4Team' in response.data
