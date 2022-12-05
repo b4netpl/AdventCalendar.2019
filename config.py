44d221718f365aa7f250a907c703daf32dd1ae5b
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-12-05 02:49:54 +0100

    added new Config object to config

--- config.py
+++ config.py
@@ -9,3 +9,8 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
