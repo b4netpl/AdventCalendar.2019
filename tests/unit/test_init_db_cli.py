bf93c43681576bcf1551c8772e9d89bf5c4c34fb
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-12-06 03:45:59 +0100

    added empty-calendar and help-load unit tests

--- tests/unit/test_init_db_cli.py
+++ tests/unit/test_init_db_cli.py
@@ -1,3 +1,6 @@
+import json
+import os.path
+
 from advencal.models import User, Day, Help
 
 
@@ -20,7 +23,12 @@ def test_app_group_help(runner_client):
     assert 'empty-calendar' in runner_client.invoke(args='init-data').output
 
 
-def test_create_user(runner_client, init_database, caplog):
+def test_create_user(runner_client, init_database):
+    """
+    GIVEN flask cli app
+    WHEN create-user command is used
+    THEN user with provided username and password is created
+    """
     result = runner_client.invoke(args=[
             'init-data',
             'create-user',
@@ -35,7 +43,14 @@ def test_create_user(runner_client, init_database, caplog):
     user = User.query.filter_by(username='clitest').scalar()
     assert user.check_password('pass') is True
 
-    result = runner_client.invoke(args=[
+
+def test_create_user_double(runner_client, init_database, caplog):
+    """
+    GIVEN flask cli app
+    WHEN create-user command is used with existing username
+    THEN error message is written to log
+    """
+    runner_client.invoke(args=[
             'init-data',
             'create-user',
             '--username',
@@ -44,3 +59,62 @@ def test_create_user(runner_client, init_database, caplog):
             'pass'
             ])
     assert 'UNIQUE constraint failed' in caplog.text
+
+
+def test_empty_calendar_noaction(runner_client, init_database):
+    """
+    GIVEN flask cli app
+    WHEN empty-calendar command is used with default No aswer to prompt
+    THEN no data is changed in Day table
+    """
+    assert 'ERASE the calendar' in runner_client.invoke(args=[
+            'init-data',
+            'empty-calendar'
+            ], input='\n').output
+    assert Day.get_day(1).day_no == 5
+
+
+def test_empty_calendar(runner_client, init_database):
+    """
+    GIVEN flask cli app
+    WHEN empty-calendar command is used with --yes arg
+    THEN initial data is written to Day table
+    """
+    runner_client.invoke(args=['init-data', 'empty-calendar', '--yes'])
+    assert Day.get_day(1).day_no == 17
+
+
+def test_help_load_nofile(runner_client, init_database, monkeypatch):
+    """
+    GIVEN flask cli app
+    WHEN help-load command is used and file is not loaded
+    THEN error message is displayed
+    """
+    def invalid_path(*args, **kwargs):
+        return 'invalid/path'
+    monkeypatch.setattr(os.path, 'join', invalid_path)
+    assert 'Error opening file. Data not changed.' \
+        in runner_client.invoke(args=[
+                'init-data',
+                'help-load',
+                'pl',
+                '--yes'
+                ]).output
+
+
+def test_help_load(runner_client, init_database, mocker):
+    """
+    GIVEN flask cli app
+    WHEN help-load command is used
+    THEN file contents are loaded to Help table
+    """
+    read_data = json.dumps([{
+            "order": 1,
+            "title": "Tytuł z pliku",
+            "body": "Treść z pliku",
+            "admin": False
+            }])
+    mocker.patch('builtins.open', mocker.mock_open(read_data=read_data))
+    runner_client.invoke(args=['init-data', 'help-load', 'pl', '--yes'])
+    helpitem = Help.get_helpitem(1)
+    assert helpitem.title == 'Tytuł z pliku'
