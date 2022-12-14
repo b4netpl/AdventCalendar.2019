6f0cf45ac822d97ce3950884ade4ae84f851e73a
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-12-14 03:22:35 +0100

    added help edit tests

--- tests/functional/test_help.py
+++ tests/functional/test_help.py
@@ -92,3 +92,61 @@ def test_edithelp_add_item(client, init_database):
             }, follow_redirects=True)
     assert 'Tytuł 6' in response.data.decode('utf-8')
     assert 'Treść 6' in response.data.decode('utf-8')
+
+
+def test_edithelp_delete_item(client, init_database):
+    """
+    GIVEN a Flask app configured for testing
+    WHEN the '/helpedit' page is requested (POST) by admin user
+            deleting help item
+    THEN help item is not visible
+    """
+    with client.session_transaction() as sess:
+        sess['user_id'] = 2
+        sess['admin'] = True
+    response = client.post(url_for('admin.edithelp'), data={
+            'del_helpitem': 'submit',
+            'helpitem_id': 1
+            }, follow_redirects=True)
+    assert 'Tytuł 1' not in response.data.decode('utf-8')
+    assert 'Treść 1' not in response.data.decode('utf-8')
+
+
+def test_edithelp_change_item(client, init_database):
+    """
+    GIVEN a Flask app configured for testing
+    WHEN the '/helpedit' page is requested (POST) by admin user
+            changing help item
+    THEN help item has new value
+    """
+    with client.session_transaction() as sess:
+        sess['user_id'] = 2
+        sess['admin'] = True
+    response = client.post(url_for('admin.edithelp'), data={
+            'save_helpitem': 'submit',
+            'helpitem_id': 1,
+            'title': 'Tytuł zmieniony',
+            'body': 'Treść zmieniona'
+            }, follow_redirects=True)
+    assert 'Tytuł 1' not in response.data.decode('utf-8')
+    assert 'Treść 1' not in response.data.decode('utf-8')
+    assert 'Tytuł zmieniony' in response.data.decode('utf-8')
+    assert 'Treść zmieniona' in response.data.decode('utf-8')
+
+
+def test_edithelp_reorder_items(client, init_database):
+    """
+    GIVEN a Flask app configured for testing
+    WHEN the '/helpedit' page is requested (POST) by admin user
+            changing help items order
+    THEN help items are displayed in new order
+    """
+    with client.session_transaction() as sess:
+        sess['user_id'] = 2
+        sess['admin'] = True
+    response = client.post(url_for('admin.edithelp'), data={
+            'userhelp_order': '2,3,4,5,1',
+            'adminhelp_order': '6,7,8'
+            }, follow_redirects=True)
+    assert response.data.decode('utf-8').index('Tytuł 2') \
+        < response.data.decode('utf-8').index('Tytuł 1')
