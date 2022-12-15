945c49b1627780b6f4bc53c27d81aadc24f9e212
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-12-16 00:16:29 +0100

    added flask-babel and config

--- advencal/__init__.py
+++ advencal/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_babel import Babel
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+babel = Babel()
 
 
 def create_app(config_class=Config):
@@ -15,6 +17,8 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
 
+    babel.init_app(app)
+
     from advencal.basic import bp as basic_bp
     app.register_blueprint(basic_bp)
 
@@ -22,6 +26,11 @@ def create_app(config_class=Config):
     app.register_blueprint(admin_bp)
 
     return app
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
 
 from advencal import models  # noqa: F401,E402
