5c8f5b473c222413910df24658acfe92d3786f87
Bartek Brzeziński <13765657+bartekbrzezinski@users.noreply.github.com>
2022-12-21 04:29:25 +0100

    added admin_required decorator

--- advencal/helpers.py
+++ advencal/helpers.py
@@ -1,5 +1,7 @@
 from advencal import db
-from flask import current_app
+from flask import current_app, redirect, url_for
+from flask_login import current_user
+from functools import wraps
 
 
 def commit(dbsession):
@@ -8,3 +10,14 @@ def commit(dbsession):
     except db.exc.DBAPIError as ex:
         current_app.logger.error(ex.orig)
         dbsession.rollback()
+
+
+def admin_required(func):
+
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.admin:
+            return redirect(url_for('basic.index'))
+        return func(*args, **kwargs)
+
+    return decorated_view
